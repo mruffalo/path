mruffalo/path

commit 548674843934081d201a3ed359c7431b5ea2507a

Small style tweaks
* Make class names CamelCase
* Don't explicitly compare against True and False
* Use 'pass' for no-op statements instead of 0 or 1
* Remove some redundant parentheses

src/pathlang.py
@@ -22,7 +22,7 @@
 import sys
 import os
 
-class path:
+class Path:
     """Implementation of the PATH programming language in Python."""
 
     # This class uses the following data attributes:
@@ -57,7 +57,7 @@ class path:
         ret = 0
         for i in range(x):
             ret = self.step()
-            if ret == false:
+            if not ret:
                 return ret
         return ret
 
@@ -69,7 +69,7 @@ class path:
     
     def debug(self, msg):
         """Print a debug message."""
-        if self.verbose == True:
+        if self.verbose:
             self.errprint("(" + str(self.x) + "," + str(self.y) + ") " + msg)
     
     def dir2string(self, d):
@@ -152,24 +152,24 @@ class path:
     def run(self):
         """Run the entire program."""
         while self.step() == 0:
-            1
+            pass
         
     def runplugins(self):
         """Run all the loaded plugins on the current symbol."""
         for plugin in self.plugins:
-            if plugin.call(self) == False:
+            if not plugin.call(self):
                 return False
     
     def step(self):
         """Step through a single symbol of the program. Return false if end of program encountered."""
         cursym = self.prog[self.y][self.x]
 
-        if self.s == True:
+        if self.s:
             self.s = False
-        elif self.plug_lock != None:
+        elif self.plug_lock is not None:
             self.plug_lock.call(self)
-        elif self.runplugins() == False:
-            0
+        elif not self.runplugins():
+            pass
         elif cursym == '$':
             self.debug("Start")
         elif cursym == '#':
@@ -225,12 +225,12 @@ class path:
                 self.debug("New direction: " + self.dir2string(self.d))
         elif cursym == '+':
             self.mem[self.p] += 1
-            if (self.mem[self.p] == 256):
+            if self.mem[self.p] == 256:
                 self.mem[self.p] = 0
             self.debug("Incremented memory cell " + str(self.p) + " to " + str(self.mem[self.p]))
         elif cursym == '-':
             self.mem[self.p] -= 1
-            if (self.mem[self.p] == -1):
+            if self.mem[self.p] == -1:
                 self.mem[self.p] = 255
             self.debug("Decremented memory cell " + str(self.p) + " to " + str(self.mem[self.p]))
         elif cursym == ',':

examples/plugin_example.py
@@ -20,7 +20,7 @@ PATH_DIRECTION_LEFT = 3
 PATH_DIRECTION_UP = 4
 
 # Class for the plugin.
-class exampleplugin:
+class ExamplePlugin:
     """Plugin description"""
     def __init__(self):
         # Initialize stuff for your plugin here.
@@ -60,4 +60,4 @@ class exampleplugin:
             return True
 
 # This line adds the plugin to the interpreter.
-glob_path.addplugin(exampleplugin())
+glob_path.addplugin(ExamplePlugin())

plugins/fileio.py
@@ -2,7 +2,7 @@
 # This plugin lets you redirect input and output to a file.
 # (c) 2004 Francis Rogers. License is the same as that for the PATH interpreter.
 
-class fileio:
+class FileIO:
     """Plugin that redirects input and output to a file."""
     def __init__(self):
         self.file_active = False
@@ -59,4 +59,4 @@ class fileio:
         except IOError:
             return 0
 
-glob_path.addplugin(fileio())
+glob_path.addplugin(FileIO())

plugins/pypath.py
@@ -2,7 +2,7 @@
 # This plugin offers inline execution of Python expressions and statements in a PATH program.
 # (c) 2004 Francis Rogers. License is the same as that for the PATH interpreter.
 
-class pypath:
+class PyPath:
     """Plugin that allows inline execution of Python expressions and statements."""
     def __init__(self):
         self.py_buffer = ""
@@ -44,4 +44,4 @@ class pypath:
         return self.py_buffer
         
 
-glob_path.addplugin(pypath())
+glob_path.addplugin(PyPath())

plugins/string.py
@@ -2,7 +2,7 @@
 # This plugin adds string literals and string output to PATH programs.
 # (c) 2004 Francis Rogers. License is the same as that for the PATH interpreter.
 
-class string:
+class String:
     """String literals and string output plugin."""
     def __init__(self):
         self.string_active = False
@@ -11,8 +11,8 @@ class string:
 
     def call(self, pathobj):
         cursym = pathobj.prog[pathobj.y][pathobj.x]
-        if self.string_active == True:
-            if self.string_escape == True:
+        if self.string_active:
+            if self.string_escape:
                 if cursym == '\\':
                     self.string_buffer += '\\'
                 elif cursym == 'n':
@@ -58,4 +58,4 @@ class string:
         else:
             return True
 
-glob_path.addplugin(string())
+glob_path.addplugin(String())

setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import sys
 import os
 from distutils.core import setup
 
@@ -16,8 +15,8 @@ setup(name="path",
       author_email="exorcismtongs@users.sf.net",
       url="http://pathlang.sf.net/",
       package_dir={'': 'src'},
-      py_modules = ['pathlang'],
-      scripts = ['src/path'],
+      py_modules=['pathlang'],
+      scripts=['src/path'],
       data_files=[('/usr/share/man/man1/', ['doc/path.1']),
                   ('/usr/lib/path/', plugins)]
     )

src/path
@@ -24,7 +24,7 @@
 import sys
 import os
 import getopt
-from pathlang import path
+from pathlang import Path
 
 def path_main():
     """Command line interpreter for the PATH programming language."""
@@ -50,17 +50,17 @@ def path_main():
     if len(args) == 0:
         path_usage()
     for f in args:
-        prog = path()
+        prog = Path()
         prog.load_prog_file(f)
         if noplugins != True:
-            path_loadplugins(prog, plugdir)
+            path_load_plugins(prog, plugdir)
         prog.verbose = verbose
         try:
             prog.run()
         except KeyboardInterrupt:
             sys.exit()
 
-def path_loadplugins(prog, plugdir):
+def path_load_plugins(prog, plugdir):
     """Load plugins from plugin directories."""
     plugindirs = ["/usr/lib/path", "~/.path"]
     

commit 91827d04e6ea3ec1ea3fd4396c5d63879f3be536

pathlang.Path.run_plugins: return True if no plugins processed the symbol
The old behavior returned either False (character was handled by a plugin) or
None (interpreter core should handle character). One of my small style tweaks
in 548674843934081d201a3ed359c7431b5ea2507a broke this, so change the return
values to True/False instead of None/False.

src/pathlang.py
@@ -159,6 +159,7 @@ class Path:
         for plugin in self.plugins:
             if not plugin.call(self):
                 return False
+        return True
     
     def step(self):
         """Step through a single symbol of the program. Return false if end of program encountered."""